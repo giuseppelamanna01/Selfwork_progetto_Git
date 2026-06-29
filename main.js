40792b322f208f83002f6081f67109233c574ae3
Giuseppe Lamanna <g.lamanna2021@gmail.com>
2026-06-29 15:05:17 +0200

    Esercizio modificato e salvato con i comandi gitbash

---
+++ main.js
@@ -0,0 +1,18 @@
+// Creazione di 2 variabili
+let num1 = 15;
+let num2 = 35;
+
+console.log(num1, num2);
+
+// Creazione variabile 'stringa'
+let stringa = "Questa è una stringa";
+// console.log(stringa);
+
+// Sostituzione del valore della variabile 'stringa' con il valore 5
+stringa = 5;
+console.log(stringa);
+
+// Dichiarazione costante
+const PIGRECO = 3.14159265359;
+console.log(PIGRECO);
+PIGRECO = 7;
